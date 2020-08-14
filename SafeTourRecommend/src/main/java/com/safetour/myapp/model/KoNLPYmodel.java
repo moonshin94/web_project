17aeefa1dbe6f33e796732990220bf50c67a97bb
moonshin94 <68261413+moonshin94@users.noreply.github.com>
2020-08-14 11:22:50 +0900

    자바와 KoNLpy 모델 연동

---
+++ SafeTourRecommend/src/main/java/com/safetour/myapp/model/KoNLPYmodel.java
@@ -0,0 +1,13 @@
+package com.safetour.myapp.model;
+
+import org.python.util.PythonInterpreter;
+
+public class KoNLPYmodel {
+	public String ThemeClassifier(String Sentence) {
+		PythonInterpreter = new PythonInterpreter();
+		
+		
+		return "";
+		
+	}
+}
